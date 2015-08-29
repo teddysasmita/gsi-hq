6e61eb97794489ddc1da9438593379801c24cf3b
teddysasmita <teddie.sasmita@gmail.com>
2015-08-29 11:47:01 +0700

    Signed-off-by: teddysasmita <teddie.sasmita@gmail.com>

--- protected/modules/stockexits/views/detailstockexits/_form.php
+++ protected/modules/stockexits/views/detailstockexits/_form.php
@@ -56,26 +56,27 @@ $supplierScript=<<<EOS
    			var myserialnum = $('#Detailstockexits_serialnum').val();
    			if (myserialnum !== 'Belum Diterima') {
 	   			$.getJSON('index.php?r=LookUp/checkItemSerial', 
-   				{ iditem: escape($('#Detailstockexits_iditem').val()), 
-   				serialnum: escape($('#Detailstockexits_serialnum').val()), 
-   				idwh: escape($('#idwh').val())  },
-   				function(data) {
-   				if ((data == false)) {
-   					$('#status').removeClass('money');
-   					$('#status').addClass('error');
-   					$('#status').html('Barang dan Nomor seri tidak ditemukan');
-   					$('#Detailstockexits_status').val('');
-   				} else if (data.status == '0'){
-   					$('#status').removeClass('money');
-   					$('#status').addClass('error');
-   					$('#status').html('Barang rusak');
-   					$('#Detailstockexits_status').val('0');
-   				} else if (data.status == '1') {
-   					$('#Detailstockexits_status').val("1");
-   					$('#status').removeClass('error');
-   					$('#status').addClass('money');
-   					$('#status').html('Bagus');
-   				}			
+	   				{ iditem: escape($('#Detailstockexits_iditem').val()), 
+	   				serialnum: escape($('#Detailstockexits_serialnum').val()), 
+	   				idwh: escape($('#idwh').val())  },
+	   				function(data) {
+	   				if ((data == false)) {
+	   					$('#status').removeClass('money');
+	   					$('#status').addClass('error');
+	   					$('#status').html('Barang dan Nomor seri tidak ditemukan');
+	   					$('#Detailstockexits_status').val('');
+	   				} else if (data.status == '0'){
+	   					$('#status').removeClass('money');
+	   					$('#status').addClass('error');
+	   					$('#status').html('Barang rusak');
+	   					$('#Detailstockexits_status').val('0');
+	   				} else if (data.status == '1') {
+	   					$('#Detailstockexits_status').val("1");
+	   					$('#status').removeClass('error');
+	   					$('#status').addClass('money');
+	   					$('#status').html('Bagus');
+   						$('#detailstockexits-form').submit();
+	   				}			
    			});
    		}
    	});
