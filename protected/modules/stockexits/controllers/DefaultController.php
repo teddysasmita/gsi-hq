9c858ed3246d01c79e141100c7ce0e02dc85b6b8
Teddy Sasmita <teddie.sasmita@gmail.com>
2015-11-27 16:48:43 +0700

    Signed-off-by: Teddy Sasmita <teddie.sasmita@gmail.com>

--- protected/modules/stockexits/controllers/DefaultController.php
+++ protected/modules/stockexits/controllers/DefaultController.php
@@ -767,8 +767,9 @@ EOS;
 			->select('a.id, b.iditem, sum(b.qty) as qty')
 			->from('orderretrievals a')
 			->join('detailorderretrievals b', 'b.id=a.id')
-			->where('a.regnum = :p_regnum and b.idwarehouse = :p_idwarehouse',
-					array(':p_regnum'=>$nolpb, ':p_idwarehouse'=> $idwh) )
+			>join('salespos c', 'c.regnum = a.invnum')
+			->where('a.regnum = :p_regnum and b.idwarehouse = :p_idwarehouse and c.status = :p_status',
+					array(':p_regnum'=>$nolpb, ':p_idwarehouse'=> $idwh, ':p_status'=>'1') )
 					->group('b.iditem')
 					->queryAll();
 		} else
